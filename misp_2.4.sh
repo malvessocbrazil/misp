6f9c8fc03bf68071fe2c0c282221bc64f8c2b732
malvessocbrazil <malves@socbrazil.com>
2025-11-01 22:42:37 -0300

    Misp versão 2.4 - V1.0.2

--- misp_2.4.sh
+++ misp_2.4.sh
@@ -3,7 +3,7 @@ set -e # Encerra o script imediatamente se um comando falhar
 
 echo "Iniciando a instalação automatizada do MISP..."
 
-# 1. Baixar o script de instalação oficial (com a URL corrigida)
+# 1. Baixar o script de instalação oficial 
 echo "Baixando o script de instalação..."
 wget --no-cache -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
 
@@ -11,7 +11,7 @@ wget --no-cache -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2
 echo "Mudando para o diretório /tmp..."
 cd /tmp
 
-# 3. Executar o script de instalação com a flag -c
+# 3. Executar o script de instalação com a flag -c (Core) 
 echo "Executando o instalador do MISP..."
 bash INSTALL.sh -c
 
